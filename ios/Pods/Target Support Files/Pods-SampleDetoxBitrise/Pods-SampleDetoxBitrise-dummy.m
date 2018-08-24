1545fd082891a8e2b1868b49cd9ea2f8edc4261f
Pat Needham <pat@needham.io>
2018-08-24 17:26:59 -0400

    with AppCenter config

---
+++ ios/Pods/Target Support Files/Pods-SampleDetoxBitrise/Pods-SampleDetoxBitrise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleDetoxBitrise : NSObject
+@end
+@implementation PodsDummy_Pods_SampleDetoxBitrise
+@end
---
+++ ios/Pods/Target Support Files/Pods-SampleDetoxBitriseTests/Pods-SampleDetoxBitriseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleDetoxBitriseTests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleDetoxBitriseTests
+@end
