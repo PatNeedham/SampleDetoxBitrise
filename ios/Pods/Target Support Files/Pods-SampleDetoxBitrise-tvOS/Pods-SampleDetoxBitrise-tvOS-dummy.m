857742d53b803868c4103925e84f11466083fa09
Pat Needham <pat@needham.io>
2018-08-27 12:13:06 -0400

    remove AppCenter-related stuff

--- ios/Pods/Target Support Files/Pods-SampleDetoxBitrise-tvOS/Pods-SampleDetoxBitrise-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleDetoxBitrise_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_SampleDetoxBitrise_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-SampleDetoxBitrise-tvOSTests/Pods-SampleDetoxBitrise-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleDetoxBitrise_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleDetoxBitrise_tvOSTests
-@end
